d093f461191af2c1f6bce3f0984d6a834ea196ff
Napat <elecwebmaker@gmail.com>
2020-03-19 21:02:24 +0700

    remove console.log

--- src/services/case.js
+++ src/services/case.js
@@ -4,7 +4,6 @@ import apiUrlProvider, { CASE } from 'constants/api-endpoints';
 export const getCases = () => {
   const url = apiUrlProvider.get(CASE.LIST);
   return fetchGet(url).then(response => {
-    console.log('response', response);
     return response.data;
   });
 };
