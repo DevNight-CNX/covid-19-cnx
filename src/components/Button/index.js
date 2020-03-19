7b9e0805ffbfc5df812afb2a93a67b3d4f90e19a
Teerapat Injai <teerapat.man19@hotmail.com>
2020-03-19 21:07:50 +0700

    implement style button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,34 +1,49 @@
 import styled from 'styled-components';
-import { provideVariant } from 'utils/style';
+import { Button } from 'antd';
 
-const size = provideVariant('size', {
-  small: '8px 0',
-  medium: '14px 0'
-});
+const Buttons = styled(Button)`
+  && {
+    ${({ theme }) => theme.typography.button()};
+    background: ${({ theme, type }) =>
+      type ? null : theme.color.primaryColor.blueRibbon};
+    color: ${({ theme, type }) =>
+      type
+        ? theme.color.primaryColor.blueRibbon
+        : theme.color.neutralColor.white};
+    border: 1px solid ${({ theme }) => theme.color.primaryColor.blueRibbon};
+    border-radius: 4px;
 
-const ButtonDefaultProps = {
-  size: 'medium'
-};
-
-const Button = styled.button`
-  width: 100%;
-  color: ${({ theme }) => theme.elementColor.buttonText};
-  background-color: ${({ solid, theme }) =>
-    !solid ? 'transparent' : theme.elementColor.button};
-  border: 2px solid ${({ theme }) => theme.elementColor.buttonOutline};
-  padding: ${size};
-  cursor: pointer;
-  border-radius: 4px;
-  transition: all 0.3s;
-
-  ${({ theme }) => theme.typography.button()}
-
-  &:hover {
-    background-color: ${({ theme }) => theme.elementColor.button};
-    border-color: ${({ theme }) => theme.elementColor.button};
+    :hover {
+      background: ${({ theme, type }) =>
+        type ? null : theme.color.alternativeColors.blueRibbonDark};
+      color: ${({ theme, type }) =>
+        type
+          ? theme.color.alternativeColors.blueRibbonDark
+          : theme.color.neutralColor.white};
+      border: 1px solid
+        ${({ theme }) => theme.color.alternativeColors.blueRibbonDark};
+    }
+    :active {
+      background: ${({ theme, type }) =>
+        type ? null : theme.color.alternativeColors.blueRibbonDark};
+      color: ${({ theme, type }) =>
+        type
+          ? theme.color.alternativeColors.blueRibbonDark
+          : theme.color.neutralColor.white};
+      border: 1px solid
+        ${({ theme }) => theme.color.alternativeColors.blueRibbonDark};
+    }
+    :focus {
+      background: ${({ theme, type }) =>
+        type ? null : theme.color.alternativeColors.blueRibbonDark};
+      color: ${({ theme, type }) =>
+        type
+          ? theme.color.alternativeColors.blueRibbonDark
+          : theme.color.neutralColor.white};
+      border: 1px solid
+        ${({ theme }) => theme.color.alternativeColors.blueRibbonDark};
+    }
   }
 `;
 
-Button.defaultProps = ButtonDefaultProps;
-
-export default Button;
+export default Buttons;
