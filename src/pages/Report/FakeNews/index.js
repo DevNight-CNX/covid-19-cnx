fa8ff6fac731ce48173d74b2d5f7c31c23b3de06
GolfKlub <J.jumpatong@hotmail.com>
2020-03-22 20:29:16 +0700

    revesre report and fix avatar image

--- src/pages/Report/FakeNews/index.js
+++ src/pages/Report/FakeNews/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import FakeNewCount from './FakeNewCount';
 import Explanation from './Explanation';
+import styled from 'styled-components';
+import FakeNewsIcon from './assets/FakeNews.svg';
+
+const FakeNewsWrapper = styled.div`
+  ${({ theme }) => theme.typography.body()};
+  color: ${({ theme }) => theme.color.error};
+  background-color: ${({ theme }) => theme.color.neutralColor.background};
+  margin-top: 3px;
+  padding: 8px 0;
+  position: relative;
+  text-align: center;
+`;
+const ExplanationWrapper = styled.div`
+  ${({ theme }) => theme.typography.body()};
+  background-color: ${({ theme }) => theme.color.secondaryColor.gold};
+  padding: 24px 26px;
+`;
+ExplanationWrapper.Title = styled.div`
+  ${({ theme }) => theme.typography.bodyLarge()};
+  color: ${({ theme }) => theme.color.neutralColor.black};
+  margin-bottom: 8px;
+`;
+ExplanationWrapper.Content = styled.div``;
 
 const FakeNewsReport = () => {
   return (
