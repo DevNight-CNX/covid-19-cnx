c1a69ca009de14a7264387e1a02ba5d4218972a9
GolfKlub <J.jumpatong@hotmail.com>
2020-03-23 19:23:29 +0700

    remove unused code

--- src/pages/Home/components/News/index.js
+++ src/pages/Home/components/News/index.js
@@ -40,7 +40,7 @@ const News = () => {
           <CardsLoading rows={2} />
         ) : (
           <>
-            {reliableReports.length === 99 ? (
+            {reliableReports.length ? (
               <Slider {...settings}>
                 {reliableReports.map((report, i) => {
                   if (i % 2) return null;
