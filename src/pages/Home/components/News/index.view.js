9c379635357fc0c894a7b904e5b8f273eda2723a
GolfKlub <J.jumpatong@hotmail.com>
2020-03-23 19:12:14 +0700

    empty state ui

--- src/pages/Home/components/News/index.view.js
+++ src/pages/Home/components/News/index.view.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import styled from 'styled-components';
+import { ReactComponent as Subtract } from './assets/subtract.svg';
 
 const Wrapper = styled.div`
   padding: 16px;
@@ -7,6 +9,8 @@ const Wrapper = styled.div`
   overflow: hidden;
 `;
 const ButtonsWrapper = styled.div`
+  height: 209px;
+  padding-top: 30px;
   display: flex;
   justify-content: center;
   margin-top: 25px;
@@ -18,13 +22,40 @@ const WrapperCards = styled.div`
   margin-top: 15px;
 `;
 
-const EmptyState = styled.div`
-  height: 200px;
+const EmptyIcon = styled(Subtract)``;
+
+const EmptyWrapper = styled.div`
   width: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   ${({ theme }) => theme.typography.body()}
 `;
+
+const EmptyTitle = styled.div`
+  margin: 15px auto 4px;
+`;
+
+const EmptyDescription = styled.div`
+  max-width: 307px;
+  ${({ theme }) => theme.typography.bodySmallHighlight()};
+  text-align: center;
+`;
+
+const EmptyState = () => {
+  return (
+    <EmptyWrapper>
+      <EmptyIcon />
+      <EmptyTitle>ยังไม่มีข้อมูล</EmptyTitle>
+      <EmptyDescription>
+        กำลังประมวลผล และคัดกรองข่าวจากชุมชน <br />
+        คุณสามารถเป็นส่วนนึงของการคัดกรองข่าว
+        <br />
+        โดยการโหวตความน่าเชื่อถือของข่าวได้ ในส่วนของ “ข่าวทั้งหมด”
+      </EmptyDescription>
+    </EmptyWrapper>
+  );
+};
 
 export { Wrapper, ButtonsWrapper, WrapperCards, EmptyState };
