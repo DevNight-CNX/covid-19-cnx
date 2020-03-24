dd122baadbf4c4b53c2a0fa43990ef2d52178723
Teerapat Injai <teerapat.man19@hotmail.com>
2020-03-24 16:32:14 +0700

    set padding loading report

--- src/pages/Home/components/News/index.js
+++ src/pages/Home/components/News/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Typography from 'components/Typography';
 import SubmitReportButton from 'components/SubmitReportButton';
