e454c0677237948f1250e74ecc05d27a57fb6e30
Napat <elecwebmaker@gmail.com>
2020-03-26 12:48:17 +0700

    implement popup for hospital

--- src/components/Map/components/IconDetail.js
+++ src/components/Map/components/IconDetail.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import newsIcon from '../assets/news.svg';
-import rippleIcon from '../assets/ripple.svg';
+import hospitalIcon from '../assets/hospital.svg';
 
 const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.color.white};
-  padding: 0px 2px;
+  padding: 10px 14px;
   bottom: 16px;
   right: 16px;
   position: absolute;
-  padding-top: 11px;
-  padding-right: 14px;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
   border-radius: 4px;
+
+  & > div:first-of-type {
+    margin-bottom: 8px;
+  }
 `;
 
 const IconWrapper = styled.div`
@@ -20,9 +22,9 @@ const IconWrapper = styled.div`
   align-items: center;
 `;
 
-const NewsIconWrapper = styled(IconWrapper)`
-  padding-left: 9px;
-`;
+// const NewsIconWrapper = styled(IconWrapper)`
+//   padding-left: 9px;
+// `;
 
 const Icon = styled.img`
   width: ${({ width }) => width}px;
@@ -32,21 +34,18 @@ const Detail = styled.p`
   ${({ theme }) => theme.typography.bodySmall()}
   color: ${({ theme }) => theme.color.neutralColor.darkGray};
   margin: 0;
-`;
-
-const NewsDetail = styled(Detail)`
   margin-left: 8px;
 `;
 
 const IconDetail = () => {
   return (
     <Wrapper>
-      <NewsIconWrapper>
-        <Icon src={newsIcon} width={21} />
-        <NewsDetail>แหล่งข่าว</NewsDetail>
-      </NewsIconWrapper>
       <IconWrapper>
-        <Icon src={rippleIcon} width={38} />
+        <Icon src={newsIcon} width={21} />
+        <Detail>แหล่งข่าว</Detail>
+      </IconWrapper>
+      <IconWrapper>
+        <Icon src={hospitalIcon} width={21} />
         <Detail>ผู้ป่วย</Detail>
       </IconWrapper>
     </Wrapper>
