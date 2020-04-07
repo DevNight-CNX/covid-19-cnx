57ddc7ea3717e26ad7f1ae380274e0ee47e18183
Napat <elecwebmaker@gmail.com>
2020-04-07 18:01:44 +0700

    implement update version for PWA cache

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
 import 'moment/locale/th';
+import { notification, Button } from 'antd';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'url-polyfill';
@@ -13,4 +14,39 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onUpdate: serviceWorkerRegistration => {
+    const key = 'version-update';
+
+    notification.open({
+      message: `มีเวอร์ชั่นอัพเดทใหม่`,
+      description:
+        'เวอร์ชั่นจะอัพเดทใหม่อัตโนมัติเมื่อปิดเว็บไซต์หรือกดปุ่ม "อัพเดท" ด้านล่าง',
+      btn: (
+        <Button
+          type="primary"
+          size="small"
+          onClick={() => {
+            notification.close(key);
+            if (serviceWorkerRegistration.waiting) {
+              serviceWorkerRegistration.waiting.postMessage({
+                type: 'SKIP_WAITING'
+              });
+              serviceWorkerRegistration.waiting.addEventListener(
+                'statechange',
+                event => {
+                  if (event.target.state === 'activated') {
+                    window.location.reload();
+                  }
+                }
+              );
+            }
+          }}
+        >
+          อัพเดท
+        </Button>
+      ),
+      key
+    });
+  }
+});
